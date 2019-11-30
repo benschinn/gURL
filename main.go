f0e56a44af84b19a5ed6a9d2e568cc1a26164b56
Ben Schinn <schinn.ben@gmail.com>
2019-11-30 16:43:41 -0700

    parsing body

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "net/http"
-  "net/http/cookiejar"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"os"
 )
 
 func main() {
-  jar, _ := cookiejar.New(nil)
-  url := os.Args[1]
-  resp, err := http.Get(url)
-  if err != nil {
-    fmt.Println("Had a problem making request!", err.Error())
-  }
-  fmt.Println(resp)
+	jar, _ := cookiejar.New(nil)
+	url := os.Args[1]
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Had a problem making request!", err.Error())
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	html := string(body)
+	fmt.Println(html)
+	fmt.Println(jar)
+	fmt.Println(resp)
 }
