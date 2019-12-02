6208676a91b826a71dd629d267e2fc008ff15d06
Ben Schinn <schinn.ben@gmail.com>
2019-12-02 09:37:54 -0700

    authenticate wip

--- main.go
+++ main.go
@@ -4,13 +4,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+  //"net/http/httputil"
 	"os"
 	"regexp"
+  //"strings"
+  //"net/url"
+  //"bytes"
+  //"log"
+  //"os/exec"
 )
 
+type UserCred struct {
+  email string
+  password string
+}
+
+var signInUrl string = os.Args[1]
+
 func getToken() string {
-	url := os.Args[1]
-	resp, err := http.Get(url)
+	resp, err := http.Get(signInUrl)
 
 	if err != nil {
 		fmt.Println("Had a problem making request!", err.Error())
@@ -34,9 +46,66 @@ func getToken() string {
 
 	return token
 }
+/*
+func authenticate(csrfToken string) {
+  requestURL := url.URL{
+    Scheme: "https",
+    Host:   "dashboard.qa.internal.mx",
+    Path:   "/sign_in",
+  }
+  reqBody := strings.NewReader(`{
+    "v1_analytics_user": {
+      "email":   {"demo@dashboard.com"},
+      "password: {"password0"},
+    },
+    "authenticity_token": {csrfToken}
+  }`)
+
+  requestHeaders := http.Header{
+    "Content-Type": {"application/vnd.moneydesktop.sso.v3+json"},
+    "Accept":       {"application/x-www-form-urlencoded"},
+    "X-CSRF-Token":   {csrfToken},
+  }
+
+  jsonBody := []byte(string(reqBody))
+
+  request := http.Request{
+    Method:        "POST",
+    URL:           &requestURL,
+    Header:        requestHeaders,
+    Body:          ioutil.NopCloser(bytes.NewReader(jsonBody)),
+    ContentLength: int64(len(jsonBody)),
+  }
+  dump, err := httputil.DumpRequest(&request, true)
+
+  if err != nil {
+    fmt.Println("dump err", err.Error())
+  }
+
+  // Make the request
+  fmt.Println("******** REQUEST ********")
+  fmt.Println(string(dump))
+}
+*/
 
 func main() {
 	csrf := getToken()
-
-	fmt.Println(csrf)
+  fmt.Println(csrf)
+  /*
+  //authenticate(csrf)
+  //cmd := exec.Command("curl", "https://dashboard.qa.internal.mx/sign_in")
+  curlReq := "https://dashboard.qa.internal.mx/sign_in"
+  userCred := "v1_analytics_user[email]=demo@dashboard.com&v1_analytics_user[password]=password0"
+  token :="authenticity_token=\"" + csrf + "\""
+  fmt.Println(curlReq)
+  cmd := exec.Command("curl", curlReq, "--data", userCred, "--data-urlencode", token, "--cookie", "cookie", "--cookie-jar", "cookie")
+  //cmd.Stdin = strings.NewReader("some input")
+  var out bytes.Buffer
+  cmd.Stdout = &out
+  err := cmd.Run()
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Printf(out.String())
+  */
 }
