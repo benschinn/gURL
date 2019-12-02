f4998cbad6da5791206f2865c89c2977c5feeddc
Ben Schinn <schinn.ben@gmail.com>
2019-12-02 14:34:32 -0700

    fmt+

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"regexp"
-  "bytes"
-  "encoding/json"
-  "time"
+	"time"
 )
 
 type UserCred struct {
-  email string
-  password string
+	email    string
+	password string
 }
 
 var signInUrl string = os.Args[1]
@@ -29,80 +29,80 @@ func getToken() string {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	html := string(body)
-  //grab meta tag with csrf token
+	//grab meta tag with csrf token
 	r := regexp.MustCompile(`<meta name="csrf-token" content=".*?(.*)\/>`)
 	metaTag := r.FindString(html)
 
-  //grab just the content attribute
+	//grab just the content attribute
 	r_ := regexp.MustCompile(`content=".*?(.*)\/>`)
 	contentAttribute := r_.FindString(metaTag)
 
-  //grab token in quotes
+	//grab token in quotes
 	r__ := regexp.MustCompile(`".*?(.*)\"`)
 	tokenInQuotes := r__.FindString(contentAttribute)
 
-  //grab token without quotes
+	//grab token without quotes
 	r___ := regexp.MustCompile(`[\w | \d].*?(.*)\=`)
 	token := r___.FindString(tokenInQuotes)
-  d1 := []byte(string(body))
-  errr := ioutil.WriteFile("./token.html", d1, 0644)
-  if errr != nil {
-    fmt.Println("error writing file")
-  }
+	d1 := []byte(string(body))
+	errr := ioutil.WriteFile("./token.html", d1, 0644)
+	if errr != nil {
+		fmt.Println("error writing file")
+	}
 
 	return token
 }
 func authenticate(csrfToken string) {
-  reqBody, err := json.Marshal(map[string]string{
-    "v1_analytics_user[email]": "demo@dashboard.com",
-    "v1_analytics_user[password]": "password0",
-    "authenticity_token": csrfToken,
-    "commit": "SIGN IN",
-  })
+	reqBody, err := json.Marshal(map[string]string{
+		"v1_analytics_user[email]":    "demo@dashboard.com",
+		"v1_analytics_user[password]": "password0",
+		"authenticity_token":          csrfToken,
+		"commit":                      "SIGN IN",
+	})
 
-  if err != nil {
-    fmt.Println("Had a problem authenticating!", err.Error())
-  }
+	if err != nil {
+		fmt.Println("Had a problem authenticating!", err.Error())
+	}
 
-  timeout := time.Duration(7 * time.Second)
-  client := http.Client{
-    Timeout: timeout,
-  }
+	timeout := time.Duration(7 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
 
-  request, err := http.NewRequest("POST", signInUrl, bytes.NewBuffer(reqBody))
-  request.Header.Set("Content-type", "application/x-www-form-urlencoded")
-  request.Header.Set("Accept-language", "en-US,en;q=0.9")
+	request, err := http.NewRequest("POST", signInUrl, bytes.NewBuffer(reqBody))
+	request.Header.Set("Content-type", "application/x-www-form-urlencoded")
+	request.Header.Set("Accept-language", "en-US,en;q=0.9")
 
-  if err != nil {
-    fmt.Println("Http client has problems.", err.Error())
-  }
+	if err != nil {
+		fmt.Println("Http client has problems.", err.Error())
+	}
 
-  resp, err := client.Do(request)
+	resp, err := client.Do(request)
 
-  if err != nil {
-    fmt.Println("Error making request", err.Error())
-  }
+	if err != nil {
+		fmt.Println("Error making request", err.Error())
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    fmt.Println("Error reading body", err.Error())
-  }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading body", err.Error())
+	}
 
-  fmt.Println(csrfToken)
-  fmt.Println(bytes.NewBuffer(reqBody))
-  fmt.Println(request.Body)
-  fmt.Println(request)
-  fmt.Println(resp)
-  d1 := []byte(string(body))
-  errr := ioutil.WriteFile("./response.html", d1, 0644)
-  if errr != nil {
-    fmt.Println("error writing file")
-  }
+	fmt.Println(csrfToken)
+	fmt.Println(bytes.NewBuffer(reqBody))
+	fmt.Println(request.Body)
+	fmt.Println(request)
+	fmt.Println(resp)
+	d1 := []byte(string(body))
+	errr := ioutil.WriteFile("./response.html", d1, 0644)
+	if errr != nil {
+		fmt.Println("error writing file")
+	}
 }
 
 func main() {
 	csrf := getToken()
-  authenticate(csrf)
+	authenticate(csrf)
 }
