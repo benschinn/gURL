30cd2d2eb87a6a66f6a95226018fc68c83f8b4bc
Ben Schinn <schinn.ben@gmail.com>
2019-12-01 20:44:58 -0700

    extracting csrf token from body

--- main.go
+++ main.go
@@ -4,21 +4,41 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/http/cookiejar"
 	"os"
+	"regexp"
 )
 
+func extractToken(html string) string {
+	r := regexp.MustCompile(`<meta name="csrf-token" content=".*?(.*)\/>`)
+	metaTag := r.FindString(html)
+
+	r1 := regexp.MustCompile(`content=".*?(.*)\/>`)
+	partOfTag := r1.FindString(metaTag)
+
+	r2 := regexp.MustCompile(`".*?(.*)\"`)
+	token := r2.FindString(partOfTag)
+
+	r3 := regexp.MustCompile(`[\w | \d].*?(.*)\=`)
+	csrfToken := r3.FindString(token)
+
+	return csrfToken
+
+}
+
 func main() {
-	jar, _ := cookiejar.New(nil)
 	url := os.Args[1]
 	resp, err := http.Get(url)
+
 	if err != nil {
 		fmt.Println("Had a problem making request!", err.Error())
 	}
+
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	html := string(body)
-	fmt.Println(html)
-	fmt.Println(jar)
-	fmt.Println(resp)
+
+	csrf := extractToken(html)
+
+	fmt.Println(csrf)
 }
