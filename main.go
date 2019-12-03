90a37c715b3da583017ec38518bd06fab430b8b3
Ben Schinn <schinn.ben@gmail.com>
2019-12-02 23:24:33 -0700

    passing in csrfToken correctly

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
+	//"encoding/json"
+  "net/http/httputil"
+  "net/url"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"regexp"
-	"time"
 )
 
 var signInUrl string = os.Args[1]
@@ -48,49 +49,56 @@ func getToken() string {
 	return token
 }
 func authenticate(csrfToken string) {
-	reqBody, err := json.Marshal(map[string]string{
+  requestURL := url.URL{
+    Scheme: "http",
+    Host:   "dashboard.localhost:3000",
+    Path:   "/sign_in",
+  }
+
+  // Build the headers
+  requestHeaders := http.Header{
+    "Content-Type": {"application/x-www-form-urlencoded"},
+    "Accept-Language": {"en-US,en;q=0.9"},
+  }
+  var reqBody = []byte(fmt.Sprintf(`{
 		"v1_analytics_user[email]":    "demo@dashboard.com",
 		"v1_analytics_user[password]": "password0",
-		"authenticity_token":          csrfToken,
+		"authenticity_token":          %s,
 		"commit":                      "SIGN IN",
-	})
+	}`, csrfToken))
 
-	if err != nil {
-		fmt.Println("Had a problem authenticating!", err.Error())
-	}
+  request := http.Request{
+    Method:        "POST",
+    URL:           &requestURL,
+    Header:        requestHeaders,
+    Body:          ioutil.NopCloser(bytes.NewReader(reqBody)),
+    ContentLength: int64(len(reqBody)),
+  }
+  dump, err := httputil.DumpRequest(&request, true)
+  if err != nil {
+    fmt.Println("dump err", err.Error())
+  }
 
-	timeout := time.Duration(7 * time.Second)
-	client := http.Client{
-		Timeout: timeout,
-	}
+  // Make the request
+  fmt.Println("******** REQUEST ********")
+  fmt.Println(string(dump))
+  resp, err := http.DefaultClient.Do(&request)
 
-	request, err := http.NewRequest("POST", signInUrl, bytes.NewBuffer(reqBody))
-	request.Header.Set("Content-type", "application/x-www-form-urlencoded")
-	request.Header.Set("Accept-language", "en-US,en;q=0.9")
+  defer resp.Body.Close()
 
-	if err != nil {
-		fmt.Println("Http client has problems.", err.Error())
-	}
+  if err != nil {
+    fmt.Println("Request didn't make it!", err.Error())
+  }
 
-	resp, err := client.Do(request)
+  // Parse the response
+  responseBody, err := ioutil.ReadAll(resp.Body)
 
-	if err != nil {
-		fmt.Println("Error making request", err.Error())
-	}
+  if err != nil {
+    fmt.Println("Failed to read body", err.Error())
+  }
+  fmt.Println(resp)
 
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading body", err.Error())
-	}
-
-	fmt.Println(csrfToken)
-	fmt.Println(bytes.NewBuffer(reqBody))
-	fmt.Println(request.Body)
-	fmt.Println(request)
-	fmt.Println(resp)
-	d1 := []byte(string(body))
+	d1 := []byte(string(responseBody))
 	errr := ioutil.WriteFile("./response.html", d1, 0644)
 	if errr != nil {
 		fmt.Println("error writing file")
